9e81db2f135860e948c115d83f41ef8f3d128f23
DESKTOP-HU69O7Q\tuanchill <tuanlt203@gmail.com>
2024-12-16 18:17:06 +0700

    fix

--- resources/views/content/user/viewCourse.blade.php
+++ resources/views/content/user/viewCourse.blade.php
@@ -152,29 +152,31 @@ table {
                 # Đánh giá sản phẩm
             </div>
             @if ($check == 3)
-            <form method="post" id="form" action="{{ route('home.ratingCourse', $courses->id) }}">
+            <form class="form-rating_h" method="post" id="form" action="{{ route('home.ratingCourse', $courses->id) }}">
                 @csrf
-                <fieldset class="rating">
-                    <input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5"
-                        title="Awesome - 5 stars"></label>
-                    {{-- <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label> --}}
-                    <input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4"
-                        title="Pretty good - 4 stars"></label>
-                    {{-- <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label> --}}
-                    <input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3"
-                        title="Meh - 3 stars"></label>
-                    {{-- <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label> --}}
-                    <input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2"
-                        title="Kinda bad - 2 stars"></label>
-                    {{-- <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label> --}}
-                    <input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1"
-                        title="Sucks big time - 1 star"></label>
-                    {{-- <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label> --}}
-                </fieldset>
-                <br>
-                <div>
-                    <textarea class="comment-course" name="comment" cols="60" rows="6"
-                        placeholder="Viết ra bình luận của bạn">{{ $my_order->comment }}</textarea>
+                <div class="form-rating">
+                    <fieldset class="rating">
+                        <input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5"
+                            title="Awesome - 5 stars"></label>
+                        {{-- <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label> --}}
+                        <input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4"
+                            title="Pretty good - 4 stars"></label>
+                        {{-- <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label> --}}
+                        <input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3"
+                            title="Meh - 3 stars"></label>
+                        {{-- <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label> --}}
+                        <input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2"
+                            title="Kinda bad - 2 stars"></label>
+                        {{-- <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label> --}}
+                        <input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1"
+                            title="Sucks big time - 1 star"></label>
+                        {{-- <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label> --}}
+                    </fieldset>
+                    <br>
+                    <div class="comment-course" style="border: 1px solid #ccc;">
+                        <textarea class="comment-course" name="comment" cols="60" rows="6"
+                            placeholder="Viết ra bình luận của bạn">{{ $my_order->comment }}</textarea>
+                    </div>
                 </div>
                 <div>
                     <button class="btn-action-course submit-comment">
